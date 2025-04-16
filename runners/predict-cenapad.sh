7093c97cdd6a3ba2b16715d908ad7740be9db370
Juliana Midlej <jmesanto91@gmail.com>
2025-04-16 13:34:39 -0300

    Fix: Cenapad runners

--- runners/predict-cenapad.sh
+++ runners/predict-cenapad.sh
@@ -5,7 +5,7 @@
 
 ENV=cenapad
 SCRATCH=$HOME
-WORK_DIR=$HOME/HPA-singlecell-2nd-dual-head-pipeline
+WORK_DIR=$HOME/hpa-single-cell-2nd
 
 unset CUDA_VISIBLE_DEVICES
 # export OMP_NUM_THREADS=8
@@ -13,8 +13,8 @@ unset CUDA_VISIBLE_DEVICES
 module load python/3.8.11-gcc-9.4.0
 
 # Activate virtual environment if it exists
-echo "Activating virtual environment... ($HOME/HPA-singlecell-2nd-dual-head-pipeline/dev/bin/activate)"
-source $HOME/HPA-singlecell-2nd-dual-head-pipeline/dev/bin/activate
+echo "Activating virtual environment... ($HOME/hpa-single-cell-2nd/dev/bin/activate)"
+source $HOME/hpa-single-cell-2nd/dev/bin/activate
 
 # Navigate to the working directory
 cd $WORK_DIR
@@ -25,4 +25,4 @@ PY=python3     # path to python
 PIP=pip       # path to PIP
 
 # Train
-$PY predict.py train -i b3 -j jakiro/sin_exp5_b3_rare.yaml --predict_weights_path results/b3_F0/checkpoints/f0_epoch-18.pth
+$PY predict.py train -i b3 -j jakiro/sin_exp5_b3_rare.yaml --predict_weights_path ../results/b3_F0/checkpoints/f0_epoch-18.pth
